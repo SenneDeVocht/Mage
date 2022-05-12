0c100c896c91d6fe7e26a7ee41bc5b665aad5446
Senne De Vocht <senne.de.vocht@telenet.be>
2022-05-12 21:10:17 +0200

    Fixed Mr Hot Dog pathfinding so he doesn't walk out of the level

--- BurgerTime/MrHotDog.cpp
+++ BurgerTime/MrHotDog.cpp
@@ -29,19 +29,25 @@ void MrHotDog::FixedUpdate()
 	const auto ownPos = GetGameObject()->GetTransform()->GetWorldPosition();
 	const auto targetPos = m_pTarget->GetWorldPosition();
 	const int currentTile = m_pLevel->PositionToTileIndex(ownPos);
+	const auto currentTilePos = m_pLevel->TileIndexToPosition(currentTile);
+
+	const bool cantKeepMovingInCurrentDirection = m_CurrentDirection.x == 1 && !m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Right)
+		|| m_CurrentDirection.x == -1 && !m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Left)
+		|| m_CurrentDirection.y == 1 && !m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Up)
+		|| m_CurrentDirection.y == -1 && !m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Down);
 
 	// MOVE
 	//-----
     if (currentTile != m_LastIntersection)
     {
 	    glm::ivec2 wantedDirection{ 0, 0 };
-	    if (targetPos.x >= ownPos.x && m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Right))
+	    if (targetPos.x > ownPos.x && m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Right))
 		    wantedDirection.x = 1;
-        if (targetPos.x <= ownPos.x && m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Left))
+        if (targetPos.x < ownPos.x && m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Left))
 		    wantedDirection.x = -1;
-        if (targetPos.y >= ownPos.y && m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Up))
+        if (targetPos.y > ownPos.y && m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Up))
 		    wantedDirection.y = 1;
-	    if (targetPos.y <= ownPos.y && m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Down))
+	    if (targetPos.y < ownPos.y && m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Down))
 		    wantedDirection.y = -1;
 
 	    if (wantedDirection.y != 0 && m_CurrentDirection.y == 0)
@@ -55,6 +61,78 @@ void MrHotDog::FixedUpdate()
 			GetGameObject()->GetTransform()->SetLocalPosition(m_pLevel->SnapToPlatform(ownPos));
 			m_LastIntersection = currentTile;
         }
+	    else if (cantKeepMovingInCurrentDirection) // Cant move in current direction and also not in wanted direction
+	    {
+			// Try to find any possible direction
+
+		    if (m_CurrentDirection.x == 1)
+		    {
+			    if (m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Up))
+			    {
+				    m_CurrentDirection = { 0, 1 };
+					m_LastIntersection = currentTile;
+			    }
+				else if (m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Down))
+				{
+					m_CurrentDirection = { 0, -1 };
+					m_LastIntersection = currentTile;
+				}
+
+				else
+					assert(false); // got stuck
+		    }
+			else if (m_CurrentDirection.x == -1)
+			{
+				if (m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Up))
+				{
+					m_CurrentDirection = { 0, 1 };
+					m_LastIntersection = currentTile;
+				}
+
+				else if (m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Down))
+				{
+					m_CurrentDirection = { 0, -1 };
+					m_LastIntersection = currentTile;
+				}
+
+				else
+					assert(false); // got stuck
+			}
+			else if (m_CurrentDirection.y == 1)
+			{
+				if (m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Left))
+				{
+					m_CurrentDirection = { -1, 0 };
+					m_LastIntersection = currentTile;
+				}
+
+				else if (m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Right))
+				{
+					m_CurrentDirection = { 1, 0 };
+					m_LastIntersection = currentTile;
+				}
+
+				else
+					assert(false); // got stuck
+			}
+			else if (m_CurrentDirection.y == -1)
+			{
+				if (m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Left))
+				{
+					m_CurrentDirection = { -1, 0 };
+					m_LastIntersection = currentTile;
+				}
+
+				else if (m_pLevel->CanMoveInDirection(ownPos, Level::Direction::Right))
+				{
+					m_CurrentDirection = { 1, 0 };
+					m_LastIntersection = currentTile;
+				}
+
+				else
+					assert(false); // got stuck
+			}
+	    }
 
 	    m_pRigidBody->SetVelocity({ m_CurrentDirection.x * m_Speed, m_CurrentDirection.y * m_Speed });
 	}
